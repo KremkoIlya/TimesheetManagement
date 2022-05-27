20c6f4b2c100f46a29f421cf4104c64f6cf3475f
Ilya Kremko <kremkoilya@gmail.com>
2022-05-27 18:17:29 +0300

    TM-002 Added PingController

---
+++ TimesheetManagement.API/Controllers/PingController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TimesheetManagement.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PingController : ControllerBase
+    {
+        [HttpGet]
+        public string Ping()
+        {
+            return "pong";
+        }
+
+        [Authorize]
+        [HttpGet("auth")]
+        public string PingAuth()
+        {
+            return "auth pong";
+        }
+    }
+}
