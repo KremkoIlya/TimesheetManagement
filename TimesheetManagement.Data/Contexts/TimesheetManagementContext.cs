66318643c06a7608441e55d8a9934aae38922f93
Ilya Kremko <kremkoilya@gmail.com>
2022-05-30 09:27:18 +0300

    TM-003 Added DB context and EFCore lib to Data project

---
+++ TimesheetManagement.Data/Contexts/TimesheetManagementContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimesheetManagement.Data.Contexts
+{
+    public class TimesheetManagementContext : DbContext
+    {
+        public TimesheetManagementContext(DbContextOptions options) : base(options) { }
+    }
+}
