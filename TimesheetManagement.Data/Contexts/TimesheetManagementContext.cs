9a19aff428c2ea47c44c56bc30363f517d3e856a
Ilya Kremko <kremkoilya@gmail.com>
2022-05-30 11:41:44 +0300

    Updated SQL context to contain Timesheet entities

--- TimesheetManagement.Data/Contexts/TimesheetManagementContext.cs
+++ TimesheetManagement.Data/Contexts/TimesheetManagementContext.cs
@@ -4,11 +4,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TimesheetManagement.Domain.Entities.Timesheet;
 
 namespace TimesheetManagement.Data.Contexts
 {
     public class TimesheetManagementContext : DbContext
     {
+        public DbSet<TimesheetDay> TimesheetDays { get; set; }
+        public DbSet<TimesheetTime> TimesheetTimes { get; set; }
+
         public TimesheetManagementContext(DbContextOptions options) : base(options) { }
     }
 }
