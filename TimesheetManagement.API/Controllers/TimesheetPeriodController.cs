516800c87d01ee5d626f97196d0c1288fecead92
Ilya Kremko <kremkoilya@gmail.com>
2022-05-30 21:51:34 +0300

    Added timesheet controllers

---
+++ TimesheetManagement.API/Controllers/TimesheetPeriodController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TimesheetManagement.API.Controllers
+{
+    [ApiController]
+    [Authorize]
+    [Route("[controller]")]
+    public class TimesheetPeriodController : ControllerBase
+    {
+        public TimesheetPeriodController()
+        {
+
+        }
+    }
+}
