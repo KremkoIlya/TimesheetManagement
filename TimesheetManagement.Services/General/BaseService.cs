cdb14d116eff8daeb2027e198a8cd6c5f90a8564
Ilya Kremko <kremkoilya@gmail.com>
2022-05-30 17:36:23 +0300

    Added BaseService

---
+++ TimesheetManagement.Services/General/BaseService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TimesheetManagement.Services.DbContext;
+
+namespace TimesheetManagement.Services.General
+{
+    public class BaseService<T> : IBaseService<T> where T: class
+    {
+        public ITimesheetManagementContext Context { get ; set; }
+
+        public BaseService(ITimesheetManagementContext context)
+        {
+            Context = context;
+        }
+
+        public Task<T> GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IQueryable<T>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Add(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
---
+++ TimesheetManagement.Services/General/IBaseService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TimesheetManagement.Services.DbContext;
+
+namespace TimesheetManagement.Services.General
+{
+    public interface IBaseService<T> where T: class
+    {
+        ITimesheetManagementContext Context { get; set; }
+
+        Task<T> GetById(int id);
+
+        Task<IQueryable<T>> GetAll();
+
+        Task Add(T item);
+    }
+}
