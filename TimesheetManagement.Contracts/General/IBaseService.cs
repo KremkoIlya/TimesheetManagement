837101f011ad0e815770e3dafdbf2bfd2c46ef8a
Ilya Kremko <kremkoilya@gmail.com>
2022-06-07 11:29:44 +0300

    Added Interfaces to Contract project

---
+++ TimesheetManagement.Contracts/General/IBaseService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimesheetManagement.Contracts.General
+{
+    public interface IBaseService<T> where T : class
+    {
+        IQueryable<T> GetAll();
+        IQueryable<T> GetBy(Expression<Func<T, bool>> expression, bool trackChanges);
+        Task Create(T entity);
+        Task Update(T entity);
+        Task Delete(T entity);
+    }
+}
---
+++ TimesheetManagement.Contracts/Timesheet/ITimesheetDayService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using TimesheetManagement.Contracts.General;
+
+namespace TimesheetManagement.Contracts.Timesheet
+{
+    public interface ITimesheetDayService
+    { }
+}
