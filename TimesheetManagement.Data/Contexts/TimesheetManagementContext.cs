9357a8e5934797662ca6264db64b22be6ce6659e
Ilya Kremko <kremkoilya@gmail.com>
2022-06-07 11:50:59 +0300

    Moved data project to Data folder and removed reference to services project(updated layering)

--- TimesheetManagement.Data/Contexts/TimesheetManagementContext.cs
+++ TimesheetManagement.Data/Contexts/TimesheetManagementContext.cs
@@ -5,20 +5,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using TimesheetManagement.Domain.Entities.Timesheet;
-using TimesheetManagement.Services.DbContext;
 
 namespace TimesheetManagement.Data.Contexts
 {
-    public class TimesheetManagementContext : DbContext, ITimesheetManagementContext
+    public class TimesheetManagementContext : DbContext
     {
         public DbSet<TimesheetDay> TimesheetDays { get; set; }
         public DbSet<TimesheetPeriod> TimesheetPeriods { get; set; }
 
         public TimesheetManagementContext(DbContextOptions options) : base(options) { }
-
-        public async Task<int> SaveChanges()
-        {
-            return await base.SaveChangesAsync();
-        }
     }
 }
